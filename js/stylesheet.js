2b41c962bdcd598b86f860ab0975b3a81f1a7543
kuladeep93 <s.kuladeep93@gmail.com>
2016-08-30 12:22:41 +0530

    slider section added

--- js/stylesheet.js
+++ js/stylesheet.js
@@ -32,16 +32,62 @@ $(window).on('resize', function () {
     $wHeight = $(window).height();
     $item.height($wHeight);
 });
-$(function() {
-		var selectedClass = "";
-		$(".fil-cat").click(function(){ 
-		selectedClass = $(this).attr("data-rel"); 
-     $("#portfolio").fadeTo(100, 0.1);
-		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
-    setTimeout(function() {
-      $("."+selectedClass).fadeIn().addClass('scale-anm');
-      $("#portfolio").fadeTo(300, 1);
-    }, 300); 
-		
-	});
+$(function () {
+    var selectedClass = "";
+    $(".fil-cat").click(function () {
+        selectedClass = $(this).attr("data-rel");
+        $("#portfolio").fadeTo(100, 0.1);
+        $("#portfolio div").not("." + selectedClass).fadeOut().removeClass('scale-anm');
+        setTimeout(function () {
+            $("." + selectedClass).fadeIn().addClass('scale-anm');
+            $("#portfolio").fadeTo(300, 1);
+        }, 300);
+
+    });
+});
+
+/************************** clients slider content********************************/
+jQuery('.instagram-feed').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: true,
+    autoplay: true,
+    slidesToShow: 1,
+    slide: 'div',
+    cssEase: 'linear'
+});
+$('.client-table').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    autoplay: true,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
