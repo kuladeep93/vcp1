cb069a8a6a012fd04e862090f711ce041e1a27cf
kuladeep93 <s.kuladeep93@gmail.com>
2016-09-02 19:40:30 +0530

    pages added

--- js/stylesheet.js
+++ js/stylesheet.js
@@ -26,19 +26,6 @@ $(window).on('resize', function () {
     $wHeight = $(window).height();
     $item.height($wHeight);
 });
-//$(function () {
-//    var selectedClass = "";
-//    $(".fil-cat").click(function () {
-//        selectedClass = $(this).attr("data-rel");
-//        $("#portfolio").fadeTo(100, 0.1);
-//        $("#portfolio div").not("." + selectedClass).fadeOut().removeClass('scale-anm');
-//        setTimeout(function () {
-//            $("." + selectedClass).fadeIn().addClass('scale-anm');
-//            $("#portfolio").fadeTo(300, 1);
-//        }, 300);
-//
-//    });
-//});
 
 /************************** clients slider content********************************/
 jQuery('.instagram-feed').slick({
